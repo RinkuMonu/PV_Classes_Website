0226349cc4223553c17dc41361b27af6b49b1635
veer <veer123@gmail.com>
2025-08-25 18:24:12 +0530

    books and current affairs

--- src/components/CurrentAffairs.js
+++ src/components/CurrentAffairs.js
@@ -210,12 +210,12 @@ export default function CurrentAffairsSlider() {
             with Latest Current Affairs
           </h3>
           <div className="flex justify-center">
-  <Link
+  {/* <Link
     href="/current-affairs"
     className="bg-[#204972] text-white px-4 sm:px-6 md:px-8 py-2 cursor-pointer rounded-md whitespace-nowrap text-sm sm:text-base"
   >
     See All <span className="ml-1">→</span>
-  </Link>
+  </Link> */}
 </div>
 
         </div>
