11d49a3e2390f415bb12e9ddc4bee0c86d859d50
Rahul Singh <rahuljsrasr1112@gmail.com>
2026-09-09 10:56:03 +0530

    r

--- src/app/axios/axiosInstance.js
+++ src/app/axios/axiosInstance.js
@@ -5,7 +5,6 @@ import Swal from "sweetalert2";
 const axiosInstance = axios.create({
 
   baseURL: 'https://api.pvclasses.in/api' 
-  
   // baseURL: 'http://localhost:5006/api',
 
 // 
@@ -17,7 +16,7 @@ const axiosInstance = axios.create({
 });
 
 
-// ✅ Request Interceptor — safely set token only in browser
+// ✅ Request Interceptor — safely set token only in browser 
 axiosInstance.interceptors.request.use(
   (config) => {
     if (typeof window !== 'undefined') {
